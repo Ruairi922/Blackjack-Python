f96ffa73891a9e96f3e4bd50fbe557e459e31507
mulha <mulhallruairi@gmail.com>
2023-06-22 15:36:20 +0100

    added a player total that keeps track of players cards. added a dealers hand to eventually compare to the players total
    added money and bet

--- BlackJack.py
+++ BlackJack.py
@@ -1,9 +1,10 @@
 import random
 import time
+import sys
 
 deck = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, "King", "Queen", "Jack"]
 
-print("Welcome to blackjack!!")
+print("Welcome to BlackJack!!")
 time.sleep(1)
 print("The dealer will deal the cards now.")
 time.sleep(1)
@@ -11,11 +12,6 @@ time.sleep(1)
 player_hand = []
 dealer_hand = []
 def deal_first_cards():
-    money = 50
-    print("You have " + str(money) + " currency")
-    time.sleep(1)
-    bet = input("How much would you like to bet: ")
-    time.sleep(1)
     player_hand.append(random.choice(deck))
     player_hand.append(random.choice(deck))
     print(player_hand)
@@ -55,9 +51,6 @@ def deal_first_cards():
             print("Your total is now " + str(newtotal))
             time.sleep(1)
             print("You have gone bust")
-            time.sleep(1)
-            money -= int(bet)
-            print("Your currency is now " + str(money))
             break
 
         elif newtotal <= 21:
@@ -86,6 +79,8 @@ def deal_first_cards():
             if dealer_total <= newtotal:
                 dealer_hand.append(random.choice(deck))
                 newdealer_total = 0
+                print("Dealer pulls another card " + str(dealer_hand))
+                time.sleep(1)
                 for i in dealer_hand:
                     if i == "King" or i == "Jack" or i == "Queen":
                         newdealer_total += 10
@@ -93,17 +88,26 @@ def deal_first_cards():
                         newdealer_total += 11
                     else:
                         newdealer_total += int(i)
-
-            if newdealer_total > total and newdealer_total < 22:
-                print("dealer wins")
+                print("Dealer now has " + str(newdealer_total))
                 time.sleep(1)
-                money -= int(bet)
-                print("your money is now " + str(money))
 
-            elif newdealer_total >= 22:
-                print("YOU WIN!!")
-                time.sleep(1)
-                money += int(bet)
-                print("your money is now " + str(money))
+
+
+                if newdealer_total > newtotal and newdealer_total < 22:
+                    print("Dealer wins")
+                    time.sleep(1)
+                    sys.exit()
+
+                elif newdealer_total == 21:
+                    print("Dealer wins")
+
+
+                elif newdealer_total >= 22:
+                    print("YOU WIN!!")
+                    time.sleep(1)
+                    sys.exit()
+                break
+
+
 
 deal_first_cards()
